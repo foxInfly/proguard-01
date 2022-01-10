abd5a398f2044f6b9cca9dad5ed05e3aaaeb878d
lap_lp <lp_lap_glb@88.com>
2022-01-10 13:53:31 +0800

    Initial commit

---
+++ src/main/java/com/lp/Proguard01Application.java
@@ -0,0 +1,13 @@
+package com.lp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proguard01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proguard01Application.class, args);
+    }
+
+}
